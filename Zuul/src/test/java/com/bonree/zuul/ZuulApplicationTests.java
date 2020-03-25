901ac9b845639937f0b0a8fade472fa06899441e
李龙 <lilong@bonree.com>
2020-03-25 16:41:00 +0800

    nacos & zuul基本调试完毕

---
+++ Zuul/src/test/java/com/bonree/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonree.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
